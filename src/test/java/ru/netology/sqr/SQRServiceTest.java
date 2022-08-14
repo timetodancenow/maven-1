fba9ef5c2b102a284e458d49469a08c1b182c9f2
Карина Аллахвердиева <timetodancenow@yandex.ru>
2022-08-14 12:47:51 +0300

    Initial commit

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology.sqr;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SQRServiceTest {
+
+    @Test
+    public void shouldCalc () {
+        ru.netology.sqr.SQRService service = new ru.netology.sqr.SQRService ();
+
+        int actual = service.calcSqr ( 10, 99 );
+
+        int expected = 0;
+
+        Assert.assertEquals ( expected, actual );
+
+    }
+
+    @Test
+    public void shouldCalculate () {
+        ru.netology.sqr.SQRService service = new ru.netology.sqr.SQRService ();
+
+        int actual = service.calcSqr ( 99, 10 );
+
+        int expected = 0;
+
+        Assert.assertEquals ( expected, actual );
+
+    }
+
+}
